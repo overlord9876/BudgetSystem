3322b08fdf4eee133482c73fe1b0d8df5523b176
DESKTOP-LTMVHJ6\overlord <overlord_9876@163.com>
2018-08-29 00:09:31 +0800

    补充漏提交的文件

---
+++ Src/BudgetSystem/BudgetSystem.Bll/BaseManager.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BudgetSystem.Bll
+{
+    public abstract class BaseManager
+    {
+    }
+}
---
+++ Src/BudgetSystem/BudgetSystem/UIEntity/OperateTypes.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BudgetSystem
+{
+    public enum OperateTypes
+    {
+        New,
+        Modify,
+        Delete,
+        View,
+    }
+}
