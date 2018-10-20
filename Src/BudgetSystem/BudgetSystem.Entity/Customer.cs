1e5d642aec2b6d69e4dc6fc9ea80e6618556f9a0
Jason <328179610@qq.com>
2018-10-20 21:28:52 +0800

    更新开发进度跟踪

--- Src/BudgetSystem/BudgetSystem.Entity/Customer.cs
+++ Src/BudgetSystem/BudgetSystem.Entity/Customer.cs
@@ -6,10 +6,15 @@ using System.Data;
 namespace BudgetSystem.Entity
 {
     /// <summary>
-    /// 客户信息
+    /// 客户表
     /// </summary>
     public class Customer : IEntity
     {
+        /// <summary>
+        /// ID
+        /// </summary>
+        public int ID { get; set; }
+
         /// <summary>
         /// 客户名称
         /// </summary>
@@ -24,5 +29,32 @@ namespace BudgetSystem.Entity
         /// 录入时间
         /// </summary>
         public DateTime CreateDate { get; set; }
+
+        /// <summary>
+        /// 创建人
+        /// </summary>
+        public string CreateUser { get; set; }
+
+        /// <summary>
+        /// 创建人姓名
+        /// </summary>
+        public string CreateUserName { get; set; }
+
+        /// <summary>
+        /// 备注
+        /// </summary>
+        public string Description { get; set; }
+
+        /// <summary>
+        /// 可用状态
+        /// </summary>
+        public bool State { get; set; }
+
+        /// <summary>
+        /// 业务员列表
+        /// </summary>
+        public List<string> SalesmanList { get; set; }
+
     }
+
 }
