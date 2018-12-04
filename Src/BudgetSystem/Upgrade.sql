79473bd31f1e5d2659c9f69694355bca399e420b
Jason <Jason@DESKTOP-7SP6OTT>
2018-12-04 10:19:15 +0800

    更新脚本提交

---
+++ Src/BudgetSystem/Upgrade.sql
@@ -0,0 +1,21 @@
+
+DELIMITER ??
+DROP PROCEDURE IF EXISTS schema_change??
+CREATE PROCEDURE schema_change()
+BEGIN
+IF NOT EXISTS (SELECT * FROM information_schema.columns WHERE table_schema = DATABASE()  AND table_name = 'BankSlip' AND column_name = 'OriginalCoin2') THEN
+    ALTER TABLE BankSlip ADD COLUMN OriginalCoin2 DECIMAL null;
+		ALTER TABLE BankSlip ADD COLUMN NatureOfMoney varchar(30) null;
+END IF; 
+
+IF NOT EXISTS (SELECT * FROM information_schema.columns WHERE table_schema = DATABASE()  AND table_name = 'BudgetBill' AND column_name = 'Cus_ID') THEN
+    ALTER TABLE BudgetBill ADD COLUMN Cus_ID INT null;
+
+alter table BudgetBill add constraint FK_Reference_BudgetBill_Customer foreign key (Cus_ID)
+      references Customer (ID) on delete restrict on update restrict;
+
+END IF; 
+
+END??
+DELIMITER ;
+CALL schema_change();
